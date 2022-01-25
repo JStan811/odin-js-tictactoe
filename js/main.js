c860e14a02cf55031f8460adbb7793c632bedf0b
Jake Standley <jakestandley95@gmail.com>
2022-01-24 16:54:47 -0800

    Complete board design Here I finished designing the board in html. We have a 3x3 square grid with each cell taking up 100% height and width. I gave each cell a top and left border and then just the 3 far right borders a border right and the 3 far bottom borders a border bottom so the grid lines show.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-alert('hi');
+// Gameboard object using the Module Pattern
+
+const gameBoard = (() => {
+  let gameBoard = [];
+  return {};
+})();
+
+// Player object using a Factory Function
+
+const playerFactory = (name, symbol) => {
+  return { };
+}
+
+// Game object using the Module Pattern
+
+const game = (() => {
+
+})();
